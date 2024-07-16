c9f23d5a711c44b0755a622c41032731962ea70c
elahe aghaei <elahe.aghaee81@gmail.com>
2024-07-16 18:21:44 +0330

    added noisy imgs challenge

--- demo.py
+++ demo.py
@@ -6,7 +6,6 @@ import config as cfg
 PATH = 'path/to/selected_img'
 noisy_dst = 'path/to/noisy_images'
 
-
 model = OCR_Model(len(cfg.unique_chars)).to(cfg.device)
 
 converter = CTCLabelConverter(cfg.unique_chars, cfg.device)
